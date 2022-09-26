a55e4ebdfdf6c327ec5e9507d4225bb305720c5a
SinSungBum <ssb97015364@gmail.com>
2022-09-26 20:11:31 +0900

    2022/09/26

--- cteam/src/main/java/andmember/AndMemberDAO.java
+++ cteam/src/main/java/andmember/AndMemberDAO.java
@@ -1,17 +1,25 @@
 package andmember;
 
 
+
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import common.Common;
 
   
 @RestController
 public class AndMemberDAO {
  
 	@Autowired @Qualifier("cteam") private SqlSession sql;
+	@Autowired private Common common;
 	
+	
+	//로그인 
 	public MemberVO login(MemberVO vo, String social) {
 		//여기서 분기
 				if(social.equals("Y")) {
@@ -20,9 +28,37 @@ public class AndMemberDAO {
 					return sql.selectOne("andmember.login", vo);
 					
 				}
+				
+				
 	}//login
 	
+	//로그인시 salt 찾아주기
+	public String member_salt (String email) {
+		return sql.selectOne("andmember.salt", email);
+	}
 	
 	
+	//회원가입
+	public int join(MemberVO vo) {
+			String salt = common.generateSalt();
+			String pw = common.getEncrypt(salt, vo.getPw()); //데이터 객체에 담겨왔을 pw!
+			vo.setSalt(salt);
+			vo.setPw(pw);
+		  
+			return sql.insert("andmember.and_join", vo);
+	}
 	
+	
+	//소셜 회원가입
+	public int social_join(MemberVO vo) {
+		return sql.insert("andmember.social_join", vo);
+	}
+	
+	
+	//소셜 아이디 중복체크
+	public int social_email_check(MemberVO vo) {
+		// 결과값이 1이상이면 true , 아니면 false
+		return (Integer)sql.selectOne("andmember.social_id_check", vo);
+	}
+
 }
--- cteam/src/main/java/com/hanul/cteam/AndMemberController.java
+++ cteam/src/main/java/com/hanul/cteam/AndMemberController.java
@@ -1,38 +1,70 @@
 package com.hanul.cteam;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
 import andmember.AndMemberDAO;
 
 import andmember.MemberVO;
+import common.Common;
 
-@Controller
+@RestController
 public class AndMemberController {
 
   @Autowired AndMemberDAO dao;
+  @Autowired Common common;
 	Gson gson = new Gson();
 	
 	
 	
-	@ResponseBody @RequestMapping("/login")
+	//로그인 (salt찾아서..로그인 되게)
+	@RequestMapping("/andLogin")
 	public String login(String email, String pw, String social) {
 	   MemberVO vo = new MemberVO();
 	   if(email == null || pw == null) {
-			return gson.toJson(null);
+			//return gson.toJson(null);
 		}
+	   
+	   String salt = dao.member_salt(email);
+	   pw = common.getEncrypt(salt, pw);
 	   
 	   vo.setEmail(email);
 	   vo.setPw(pw);
-	    vo = dao.login(vo, social);
-		
+	   vo = dao.login(vo, social);
 		
 		return gson.toJson(vo);
 	}
+	
+	
+
+	//회원가입
+	@RequestMapping(value = "/andJoin", produces = "text/html;charset=utf-8")
+	public String join(String vo) {
+		MemberVO joinInfo = new Gson().fromJson(vo, MemberVO.class);
+
+		//소셜 로그인 회원가입
+		if(joinInfo.getSocial().equals("Y")) {
+			dao.social_join(joinInfo);
+		}else {
+			dao.join(joinInfo);			
+		}
+
+		return gson.toJson(vo);
+	
+	}
+	
+	
+	
+	
 	
 }
